a26e072732b1a7a171debd3a843cbc7907eb3f28
Fuzail Ahemd <fuzailcode@gmail.com>
2025-10-24 10:57:26 +0500

    fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import React, { useEffect } from 'react'
 import Home from "./Pages/Home/Home"
-import Aos from 'aos';
+import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 
 const App = () => {
 
   useEffect(() => {
-    Aos.init({
-      duration: 1000, // animation duration in ms
-      once: true,     // animation ek hi dafa chale
-      offset : 200
+    AOS.init({
+      duration: 1000,
+      once: true,
+      offset: 50,
+      mirror: false,
     });
+
+    // 🔧 Refresh after full page load
+    window.addEventListener("load", () => {
+      AOS.refresh();
+    });
+
+    return () => {
+      window.removeEventListener("load", AOS.refresh);
+    };
   }, []);
 
   return (
